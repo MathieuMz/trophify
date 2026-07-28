5d8feaaa561c527104ddd8a40bdd61ff9623d741
Mathieu Merzereau <mathieu.merzereau@ubisoft.com>
2026-07-28 12:46:51 -0400

    chore: use new schema

--- apps/api/src/lib/supabase.ts
+++ apps/api/src/lib/supabase.ts
@@ -1,7 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
+import { createClient } from "@supabase/supabase-js";
+import "dotenv/config";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
-)
+  process.env.SUPABASE_SERVICE_KEY!,
+  {
+    db: {
+      schema: "trophify",
+    },
+  },
+);
